1b494d4c70b494a62a88e4e77d6a62a1d59d427c
DennyGadow <dennygadow@web.de>
2024-11-17 22:00:14 +0100

    Added migration

---
+++ PantryTracker.Infrastructure/Migrations/20241117205745_AddProductInfoToFoodItem.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PantryTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductInfoToFoodItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Energy",
+                table: "NutritionalInfo");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Proteins",
+                table: "NutritionalInfo",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Fat",
+                table: "NutritionalInfo",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Carbohydrates",
+                table: "NutritionalInfo",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "EnergyKcal",
+                table: "NutritionalInfo",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "FoodItems",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "FoodItems",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "FoodItems",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnergyKcal",
+                table: "NutritionalInfo");
+
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "FoodItems");
+
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "FoodItems");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "FoodItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Proteins",
+                table: "NutritionalInfo",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Fat",
+                table: "NutritionalInfo",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Carbohydrates",
+                table: "NutritionalInfo",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Energy",
+                table: "NutritionalInfo",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
