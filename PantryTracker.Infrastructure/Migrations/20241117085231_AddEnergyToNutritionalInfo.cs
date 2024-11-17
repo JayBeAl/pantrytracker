1ebfe3a64ba6eba4ba64631c4b2deddc347a0e28
DennyGadow <dennygadow@web.de>
2024-11-17 10:08:50 +0100

    Added migration

---
+++ PantryTracker.Infrastructure/Migrations/20241117085231_AddEnergyToNutritionalInfo.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PantryTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEnergyToNutritionalInfo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Protein",
+                table: "NutritionalInfo",
+                newName: "Proteins");
+
+            migrationBuilder.RenameColumn(
+                name: "Calories",
+                table: "NutritionalInfo",
+                newName: "Energy");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Proteins",
+                table: "NutritionalInfo",
+                newName: "Protein");
+
+            migrationBuilder.RenameColumn(
+                name: "Energy",
+                table: "NutritionalInfo",
+                newName: "Calories");
+        }
+    }
+}
