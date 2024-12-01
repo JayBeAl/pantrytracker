033f55f22275d877b0ce7d3ba7c8eaee899d4ec8
DennyGadow <dennygadow@web.de>
2024-12-01 12:01:59 +0100

    Migrations Redone.

--- PantryTracker.Infrastructure/Migrations/20241201105631_InitialCreate.Designer.cs
+++ PantryTracker.Infrastructure/Migrations/20241201105631_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PantryTracker.Infrastructure;
 
@@ -10,9 +11,11 @@ using PantryTracker.Infrastructure;
 namespace PantryTracker.Infrastructure.Migrations
 {
     [DbContext(typeof(PantryContext))]
-    partial class PantryContextModelSnapshot : ModelSnapshot
+    [Migration("20241201105631_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.11");
