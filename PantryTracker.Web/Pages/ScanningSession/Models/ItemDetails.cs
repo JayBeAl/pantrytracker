670d0e1558d3c04421d0dddf979093a8dfb70d95
DennyGadow <dennygadow@web.de>
2024-12-01 14:24:01 +0100

    Removed old web project

--- PantryTracker.Web/Pages/ScanningSession/Models/ItemDetails.cs
+++
@@ -1,8 +0,0 @@
-namespace PantryTracker.Web.Pages.ScanningSession.Models;
-
-public class ItemDetails
-{
-    public int Quantity { get; set; } = 1;
-    public string StorageLocation { get; set; }
-    public DateTime ExpiryDate { get; set; } = DateTime.Now.AddMonths(1);
-}
--- PantryTracker.Web/Shared/Components/Alert.cs
+++
@@ -1,9 +0,0 @@
-namespace PantryTracker.Web.Shared.Components;
-
-public enum AlertType
-{
-    Success,
-    Error,
-    Warning,
-    Info
-}
